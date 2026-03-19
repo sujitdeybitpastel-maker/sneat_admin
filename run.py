5b1853def25b2b51382976dc0425b35a061ffb99
Sujit Dey <sujitdey.bitpastel@gmail.com>
2026-03-19 15:09:34 +0530

    add the run.py  file

--- run.py
+++ run.py
@@ -5,4 +5,6 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8501))
+    app.run(host="0.0.0.0", port=port, debug=False)
