55d6d102d82e34b77bb0142aa321b5f91dd7b6bd
Sujit Dey <sujitdey.bitpastel@gmail.com>
2026-03-19 15:17:13 +0530

    Chnage the port

--- run.py
+++ run.py
@@ -5,6 +5,4 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8501))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(debug=False)
